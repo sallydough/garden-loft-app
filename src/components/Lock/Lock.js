81c070abebec79167df31d6867952fe484aa6df6
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-13 14:12:06 -0700

    created home security

--- src/components/Lock/Lock.js
+++ src/components/Lock/Lock.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import "./lock.css";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+import LockIcon from "@mui/icons-material/Lock";
+import { styled } from "@mui/material";
+import Switch from '@mui/material/Switch';
+
+// const CustomSwitch = styled(Switch)({
+//   width: 80,
+// });
+
+const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
 const Lock = () => {
   return (
-    <div>Lock</div>
-  )
-}
+    <div className="lock-container">
 
-export default Lock
+      <div className="home-icon-container">
+        <h3>Home Security</h3>
+        <HomeOutlinedIcon />
+      </div>
+
+      <div className="lock-slider">
+        <LockOpenIcon className="lock" />
+        <Switch {...label} defaultChecked />
+        <LockIcon className="lock" />
+      </div>
+
+    </div>
+  );
+};
+
+export default Lock;
--- src/components/TempControl/TempControl.js
+++ src/components/TempControl/TempControl.js
@@ -4,14 +4,6 @@ import ToggleOffOutlinedIcon from "@mui/icons-material/ToggleOffOutlined";
 import ToggleOnOutlinedIcon from "@mui/icons-material/ToggleOnOutlined";
 import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
 
-const styles = {
-
-  largeIcon: {
-    width: 80,
-    height: 60,
-  },
-
-};
 
 const TempControl = () => {
   return (
