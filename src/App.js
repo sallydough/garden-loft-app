e86696d09ef587a550258b07fab1fb273ae6bf41
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-13 15:18:14 -0700

    created light toggles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import './App.css';
-import Logo from './components/Logo/Logo';
-import Call911 from './components/Call911/Call911';
-import TempControl from './components/TempControl/TempControl';
-import Lock from './components/Lock/Lock';
-import Lights from './components/Lights/Lights';
-import Greeting from './components/Greeting/Greeting';
-import Schedule from './components/Schedule/Schedule';
-
+import "./App.css";
+import Logo from "./components/Logo/Logo";
+import Call911 from "./components/Call911/Call911";
+import TempControl from "./components/TempControl/TempControl";
+import Lock from "./components/Lock/Lock";
+import Lights from "./components/Lights/Lights";
+import Greeting from "./components/Greeting/Greeting";
+import Schedule from "./components/Schedule/Schedule";
 
 function App() {
   return (
     <div className="App">
-      <Logo />
-      <Call911 />
-      <TempControl />
-      <Lock />
-      <Lights />
-      <Greeting />
-      <Schedule />
-      
-
-    </div>
+      <nav className="navbar">
+        <Logo />
+        <Call911 />
+      </nav>
+      <div className="app-layout">
+      <section className="temp-lock">
+        <TempControl />
+        <Lock />
+      </section>
+      <section className="lights">
+        <Lights />
+      </section>
+      <section className="date-schedule">
+        <Greeting />
+        <Schedule />
+      </section>
+    </div></div>
   );
 }
 
--- src/components/Lights/Lights.js
+++ src/components/Lights/Lights.js
@@ -1,9 +1,146 @@
-import React from 'react'
+import React from "react";
+import "./lights.css";
+import Switch from "@mui/material/Switch";
+import TungstenIcon from "@mui/icons-material/Tungsten";
+import { styled } from "@mui/material/styles";
+import FormControlLabel from "@mui/material/FormControlLabel";
+
+
+
+const IOSSwitch = styled((props: SwitchProps) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 42,
+  height: 26,
+  padding: 0,
+  "& .MuiSwitch-switchBase": {
+    padding: 0,
+    margin: 2,
+    transitionDuration: "300ms",
+    "&.Mui-checked": {
+      transform: "translateX(16px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        backgroundColor: theme.palette.mode === "dark" ? "#2ECA45" : "#65C466",
+        opacity: 1,
+        border: 0,
+      },
+      "&.Mui-disabled + .MuiSwitch-track": {
+        opacity: 0.5,
+      },
+    },
+    "&.Mui-focusVisible .MuiSwitch-thumb": {
+      color: "#33cf4d",
+      border: "6px solid #fff",
+    },
+    "&.Mui-disabled .MuiSwitch-thumb": {
+      color:
+        theme.palette.mode === "light"
+          ? theme.palette.grey[100]
+          : theme.palette.grey[600],
+    },
+    "&.Mui-disabled + .MuiSwitch-track": {
+      opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxSizing: "border-box",
+    width: 22,
+    height: 22,
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 26 / 2,
+    backgroundColor: theme.palette.mode === "light" ? "#E9E9EA" : "#39393D",
+    opacity: 1,
+    transition: theme.transitions.create(["background-color"], {
+      duration: 500,
+    }),
+  },
+}));
 
 const Lights = () => {
   return (
-    <div>Lights</div>
-  )
-}
+    <div className="lights-container">
+      <div className="light-icon-container">
+        <h3>Lighting Controls</h3>
+        <TungstenIcon />
+      </div>
 
-export default Lights
+      <div className="room-light">
+        <div className="house-label">
+          <h3>Living Room</h3>
+        </div>
+        <div className="light-device">
+          <p>Device 1</p>
+          <div className="light-slider">
+            <h3 className="on">On</h3>
+            <FormControlLabel
+              control={<IOSSwitch sx={{ m: 1 }} defaultChecked />}
+            />
+            <h3 className="off">Off</h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="room-light">
+        <div className="house-label">
+          <h3>Dining Room</h3>
+        </div>
+        <div className="light-device">
+          <p>Device 1</p>
+          <div className="light-slider">
+            <h3 className="on">On</h3>
+            <FormControlLabel
+              control={<IOSSwitch sx={{ m: 1 }} defaultChecked />}
+            />
+            <h3 className="off">Off</h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="room-light">
+        <div className="house-label">
+          <h3>Bedroom</h3>
+        </div>
+        <div className="light-device">
+          <p>Device 1</p>
+          <div className="light-slider">
+            <h3 className="on">On</h3>
+            <FormControlLabel
+              control={<IOSSwitch sx={{ m: 1 }} defaultChecked />}
+            />
+            <h3 className="off">Off</h3>
+          </div>
+        </div>
+        <div className="light-device">
+          <p>Device 2</p>
+          <div className="light-slider">
+            <h3 className="on">On</h3>
+            <FormControlLabel
+              control={<IOSSwitch sx={{ m: 1 }} defaultChecked />}
+            />
+            <h3 className="off">Off</h3>
+          </div>
+        </div>
+      </div>
+
+      <div className="room-light">
+        <div className="house-label">
+          <h3>Bathroom</h3>
+        </div>
+        <div className="light-device">
+          <p>Device 1</p>
+          <div className="light-slider">
+            <h3 className="on">On</h3>
+            <FormControlLabel
+              control={<IOSSwitch sx={{ m: 1 }} defaultChecked />}
+            />
+            <h3 className="off">Off</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Lights;
