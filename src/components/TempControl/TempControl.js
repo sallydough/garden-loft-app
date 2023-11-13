f888152710b7593f4a8701383a30d0173f0f13d0
Sally Do <56125850+sallydough@users.noreply.github.com>
2023-11-13 13:18:38 -0700

    created temp control

--- src/components/TempControl/TempControl.js
+++ src/components/TempControl/TempControl.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import './tempControl.css'
+import React from "react";
+import "./tempControl.css";
+import ToggleOffOutlinedIcon from "@mui/icons-material/ToggleOffOutlined";
+import ToggleOnOutlinedIcon from "@mui/icons-material/ToggleOnOutlined";
+import DeviceThermostatIcon from '@mui/icons-material/DeviceThermostat';
+
+const styles = {
+
+  largeIcon: {
+    width: 80,
+    height: 60,
+  },
+
+};
 
 const TempControl = () => {
   return (
-    <div>TempControl</div>
-  )
-}
+    <div className="temp-container">
+      <div className="temp-control">
+        <h3>Temperature</h3>
+        <h1>24&#186;C</h1>
+        <ToggleOffOutlinedIcon />
+      </div>
+      <div className="img-settings-container">
+      <div className="temp-image">
+        <DeviceThermostatIcon  />
+      </div>
+      <div className="temp-settings">
+        <button>-</button>
+        <button>+</button>
+      </div>
+      </div>
+    </div>
+  );
+};
 
-export default TempControl
+export default TempControl;
