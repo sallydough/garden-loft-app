93e84272503a985c22e8d4cc5b760529ee17283c
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-03-11 13:13:53 -0600

    create top bar logo buton welcome

--- src/components/HelpButton/HelpButton.tsx
+++ src/components/HelpButton/HelpButton.tsx
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, StyleSheet, Dimensions,TouchableOpacity } from 'react-native'
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import React from 'react'
+
+const { width: viewportWidth, height: viewportHeight } =
+  Dimensions.get("window");
 
 const HelpButton = () => {
   return (
-    <View>
-      <Text>HelpButton</Text>
+    <View style={styles.container}>
+      <Image source={require('../../../assets/images/garden-loft-logo2.png')}
+      style={{ width: 155, height: 72 }} />
+      <TouchableOpacity  style={{ backgroundColor: '#59ACCE', padding: 5, paddingLeft: 10, borderRadius: 7, flexDirection: "row", }}>
+      <MaterialCommunityIcons  
+          name="phone-classic"
+          size={52}
+          color="#f3b718"
+        />
+        <Text style={{ color: '#2E3E5E', fontSize: 30, padding: 10, }}>Call Support</Text>
+      </TouchableOpacity>
+      <Text style={{ color: '#2E3E5E', fontSize: 32, padding: 10 }}>Welcome </Text>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: viewportWidth * 0.95,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 15,
+  },
+});
 
 export default HelpButton
