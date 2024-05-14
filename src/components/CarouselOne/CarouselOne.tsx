4dd5290374fadcfa6364bec71e8c6fbefa812fee
whimsical-phoenix <shahprapti12@gmail.com>
2024-05-14 17:11:49 -0600

    remember me and sign out card added

--- src/components/CarouselOne/CarouselOne.tsx
+++ src/components/CarouselOne/CarouselOne.tsx
@@ -1,34 +1,273 @@
+// import React, { useState, useRef } from "react";
+// import {
+//   View,
+//   // Image,
+//   Text,
+//   StyleSheet,
+//   Dimensions,
+//   TouchableOpacity,
+// } from "react-native";
+// import Carousel from "react-native-snap-carousel";
+// import { MaterialCommunityIcons } from "@expo/vector-icons";
+// import { FontAwesome } from "@expo/vector-icons";
+// import VideoCall from "../CarouselTwo/VideoCall";
+// // import Activities from "../CarouselTwo/Activities";
+// import Activities2 from "../CarouselTwo/Activities2";
+// import Lights from "../CarouselTwo/Lights";
+// import Entertainment from "../CarouselTwo/Entertainment";
+// import HowTo from "../CarouselTwo/HowTo";
+// import GLCommunity from "../CarouselTwo/GLCommunity";
+// // import { FIRESTORE_DB } from "@/FirebaseConfig";
+// // import BackgroundImage from "../CarouselTwo/BackgroundImage";
+// // import VideoSDK from "../CarouselTwo/VideoSDK";
+
+// const { width: viewportWidth, height: viewportHeight } =
+//   Dimensions.get("window");
+
+// interface Item {
+//   title: string;
+//   icon: string;
+//   component?: JSX.Element;
+//   prompt: string;
+// }
+
+// const data: Item[] = [
+//   {
+//     title: "ACTIVITIES",
+//     icon: "weight-lifter",
+//     component: <Activities2 />,
+//     prompt: "Join an Activity?",
+//   },
+//   {
+//     title: "VIDEO CALL",
+//     icon: "phone",
+//     component: <VideoCall />,
+//     prompt: "Make a Video Call?",
+//   },
+//   {
+//     title: "GARDEN LOFT",
+//     icon: "home-group-plus",
+//     component: <GLCommunity />,
+//     prompt: "Meet Garden Loft Members?",
+//   },
+//   {
+//     title: "ENTERTAINMENT",
+//     icon: "movie-open-star",
+//     component: <Entertainment />,
+//     prompt: "Watch Entertainment?",
+//   },
+//   {
+//     title: "HOW-TO VIDEOS",
+//     icon: "account-question",
+//     component: <HowTo />,
+//     prompt: "Need Help With Your Garden Loft?",
+//   },
+//   {
+//     title: "LIGHTS",
+//     icon: "lightbulb",
+//     component: <Lights />,
+//     prompt: "Change Lights?",
+//   },
+// ];
+
+// const Home: React.FC = () => {
+//   const [showVideoCall, setShowVideoCall] = useState(false);
+//   const [activeIndex, setActiveIndex] = useState(0);
+
+//   const scrollViewRef = useRef<Carousel<Item>>(null);
+
+//   const handleSnapToItem = (index: number) => {
+//     setActiveIndex(index);
+//   };
+
+
+//   const handleCardPress = (index: number) => {
+//     scrollViewRef.current?.snapToItem(index);
+//   };
+
+//   const renderItem = ({ item, index }: { item: Item; index: number }) => (
+//     <TouchableOpacity onPress={() => handleCardPress(item, index)}>
+//       <View
+//         style={[
+//           styles.item,
+//           {
+//             backgroundColor: index === activeIndex + 3 ? "#f3b718" : "#909090",
+//           },
+//         ]}
+//       >
+//         <MaterialCommunityIcons
+//           style={[
+//             styles.icon,
+//             {
+//               color: index === activeIndex + 3 ? "black" : "#f3b718",
+//             },
+//           ]}
+//           name={item.icon}
+//           size={82}
+//           color="#f3b718"
+//         />
+
+//         <Text
+//           style={[
+//             styles.title,
+//             {
+//               color: index === activeIndex + 3 ? "black" : "#f3b718",
+//             },
+//           ]}
+//         >
+//           {item.title}
+//         </Text>
+//       </View>
+//       {/* <Text>{item.page}</Text> */}
+//     </TouchableOpacity>
+//   );
+
+  
+//   // const handleCardPress = (item: Item, index: number) => {
+//   //   if (item.component) {
+//   //     // Render component if available
+      
+//   //     // setShowVideoCall(false);
+//   //   } else {
+//   //     // setShowVideoCall(item.title === "VIDEO CALL");
+//   //   }
+//   //   scrollViewRef.current?.snapToItem(index);
+//   // };
+
+//   return (
+//     <View style={styles.container}>
+//        {/* <Image source={require('../../../assets/images/garden-loft-logo-outline.png')} style={{ width: 155, height: 72 }} /> */}
+//        {/* <BackgroundImage/> */}
+//       <Carousel style={styles.carousel1}
+//         layout={"default"}
+//         data={data}
+//         renderItem={renderItem}
+//         sliderWidth={Math.round(viewportWidth * 0.85)}
+//         itemWidth={Math.round(viewportWidth * 0.17)}
+//         loop={true}
+//         useScrollView={true}
+//         activeSlideAlignment="center"
+//         ref={scrollViewRef}
+//         inactiveSlideScale={0.8} // Scale of inactive slides
+//         inactiveSlideOpacity={1}
+//         onSnapToItem={(index) => handleSnapToItem(index)} // Handle snapping logic
+//       />
+//       {/* Prompt */}
+//       <Text style={styles.prompt}>{data[activeIndex].prompt && data[activeIndex].prompt}</Text>
+      
+//       {/* Bottom component */}
+//       <Text style={styles.carousel2}>{data[activeIndex].component && data[activeIndex].component}</Text>
+      
+//       {/* Arrow Keys */}
+//       <TouchableOpacity
+//         style={styles.arrowLeft}
+//         onPress={() => scrollViewRef.current?.snapToPrev()}
+//       >
+//         <FontAwesome name="angle-left" size={100} color="rgb(45, 62, 95)" />
+//       </TouchableOpacity>
+//       <TouchableOpacity
+//         style={styles.arrowRight}
+//         onPress={() => scrollViewRef.current?.snapToNext()}
+//       >
+//         <FontAwesome name="angle-right" size={100} color="rgb(45, 62, 95)" />
+//       </TouchableOpacity>
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     marginTop: 10,
+//     flexDirection: "column",
+//     gap: 5,
+//   },carousel1: {
+    
+//   },carousel2: {
+//     marginBottom: -40,
+//   },
+// prompt: {
+//     fontSize: 30,
+//     marginBottom: 15,
+//     marginTop: -10,
+//     color: "rgb(45, 62, 95)",
+//   },
+//   item: {
+//     width: viewportWidth * 0.17,
+//     height: viewportHeight * 0.25,
+//     justifyContent: "center",
+//     alignItems: "center",
+//     borderRadius: 60,
+//     flexDirection: "column",
+//     gap: 30,
+//     paddingHorizontal: 10,
+
+//     shadowColor: "#000000",
+//     shadowOffset: {
+//       width: 8,
+//       height: 7,
+//     },
+//     shadowOpacity:  0.22,
+//     shadowRadius: 9.22,
+//     elevation: 12,
+//   },
+//   title: {
+//     fontSize: 19,
+//     fontWeight: "bold",
+//     marginLeft: 10,
+//     color: "#f3b718",
+//   },
+//   arrowLeft: {
+//     position: "absolute",
+//     top: "12%",
+//     left: 0,
+//     transform: [{ translateY: -10 }],
+//   },
+//   arrowRight: {
+//     position: "absolute",
+//     top: "12%",
+//     right: -10,
+//     transform: [{ translateY: -10 }],
+//   },
+//   cardPage: {
+//     flexDirection: "column",
+//     gap: 80,
+//   },
+//   icon: {},
+// });
+
+// export default Home;
+
 import React, { useState, useRef } from "react";
 import {
   View,
-  // Image,
   Text,
   StyleSheet,
   Dimensions,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import Carousel from "react-native-snap-carousel";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { FontAwesome } from "@expo/vector-icons";
+import { MaterialCommunityIcons, FontAwesome } from "@expo/vector-icons";
+import { getAuth, signOut } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import VideoCall from "../CarouselTwo/VideoCall";
-// import Activities from "../CarouselTwo/Activities";
 import Activities2 from "../CarouselTwo/Activities2";
 import Lights from "../CarouselTwo/Lights";
 import Entertainment from "../CarouselTwo/Entertainment";
 import HowTo from "../CarouselTwo/HowTo";
 import GLCommunity from "../CarouselTwo/GLCommunity";
-// import { FIRESTORE_DB } from "@/FirebaseConfig";
-// import BackgroundImage from "../CarouselTwo/BackgroundImage";
-// import VideoSDK from "../CarouselTwo/VideoSDK";
 
-const { width: viewportWidth, height: viewportHeight } =
-  Dimensions.get("window");
+const { width: viewportWidth, height: viewportHeight } = Dimensions.get("window");
 
 interface Item {
   title: string;
   icon: string;
   component?: JSX.Element;
   prompt: string;
+  action?: () => void; // Adding action for logout
 }
 
 const data: Item[] = [
@@ -68,21 +307,54 @@ const data: Item[] = [
     component: <Lights />,
     prompt: "Change Lights?",
   },
+  {
+    title: "LOGOUT",
+    icon: "logout",
+    prompt: "Want to log out of Garden Loft app?",
+    action: async () => {
+      Alert.alert(
+        "Logout",
+        "Are you sure you want to log out?",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Logout cancelled"),
+            style: "cancel"
+          },
+          {
+            text: "Yes",
+            onPress: async () => {
+              const auth = getAuth();
+              try {
+                await signOut(auth);
+                await AsyncStorage.removeItem('rememberedUser');
+                console.log('User signed out!');
+              } catch (error) {
+                console.error('Logout failed:', error);
+              }
+            }
+          }
+        ],
+        { cancelable: false }
+      );
+    },
+  },
 ];
 
 const Home: React.FC = () => {
-  const [showVideoCall, setShowVideoCall] = useState(false);
   const [activeIndex, setActiveIndex] = useState(0);
-
   const scrollViewRef = useRef<Carousel<Item>>(null);
 
   const handleSnapToItem = (index: number) => {
     setActiveIndex(index);
   };
 
-
-  const handleCardPress = (index: number) => {
-    scrollViewRef.current?.snapToItem(index);
+  const handleCardPress = (item: Item, index: number) => {
+    if (item.action) {
+      item.action();
+    } else {
+      scrollViewRef.current?.snapToItem(index);
+    }
   };
 
   const renderItem = ({ item, index }: { item: Item; index: number }) => (
@@ -91,7 +363,7 @@ const Home: React.FC = () => {
         style={[
           styles.item,
           {
-            backgroundColor: index === activeIndex + 3 ? "#f3b718" : "#909090",
+            backgroundColor: index === activeIndex ? "#f3b718" : "#909090",
           },
         ]}
       >
@@ -99,46 +371,29 @@ const Home: React.FC = () => {
           style={[
             styles.icon,
             {
-              color: index === activeIndex + 3 ? "black" : "#f3b718",
+              color: index === activeIndex ? "black" : "#f3b718",
             },
           ]}
           name={item.icon}
           size={82}
-          color="#f3b718"
         />
-
         <Text
           style={[
             styles.title,
             {
-              color: index === activeIndex + 3 ? "black" : "#f3b718",
+              color: index === activeIndex ? "black" : "#f3b718",
             },
           ]}
         >
           {item.title}
         </Text>
       </View>
-      {/* <Text>{item.page}</Text> */}
     </TouchableOpacity>
   );
 
-  
-  // const handleCardPress = (item: Item, index: number) => {
-  //   if (item.component) {
-  //     // Render component if available
-      
-  //     // setShowVideoCall(false);
-  //   } else {
-  //     // setShowVideoCall(item.title === "VIDEO CALL");
-  //   }
-  //   scrollViewRef.current?.snapToItem(index);
-  // };
-
   return (
     <View style={styles.container}>
-       {/* <Image source={require('../../../assets/images/garden-loft-logo-outline.png')} style={{ width: 155, height: 72 }} /> */}
-       {/* <BackgroundImage/> */}
-      <Carousel style={styles.carousel1}
+      <Carousel
         layout={"default"}
         data={data}
         renderItem={renderItem}
@@ -148,17 +403,14 @@ const Home: React.FC = () => {
         useScrollView={true}
         activeSlideAlignment="center"
         ref={scrollViewRef}
-        inactiveSlideScale={0.8} // Scale of inactive slides
+        inactiveSlideScale={0.8}
         inactiveSlideOpacity={1}
-        onSnapToItem={(index) => handleSnapToItem(index)} // Handle snapping logic
+        onSnapToItem={(index) => handleSnapToItem(index)}
       />
-      {/* Prompt */}
-      <Text style={styles.prompt}>{data[activeIndex].prompt && data[activeIndex].prompt}</Text>
-      
-      {/* Bottom component */}
-      <Text style={styles.carousel2}>{data[activeIndex].component && data[activeIndex].component}</Text>
-      
-      {/* Arrow Keys */}
+      <Text style={styles.prompt}>{data[activeIndex].prompt}</Text>
+      <View style={styles.carousel2}>
+        {data[activeIndex].component}
+      </View>
       <TouchableOpacity
         style={styles.arrowLeft}
         onPress={() => scrollViewRef.current?.snapToPrev()}
@@ -182,12 +434,12 @@ const styles = StyleSheet.create({
     marginTop: 10,
     flexDirection: "column",
     gap: 5,
-  },carousel1: {
-    
-  },carousel2: {
+  },
+  carousel1: {},
+  carousel2: {
     marginBottom: -40,
   },
-prompt: {
+  prompt: {
     fontSize: 30,
     marginBottom: 15,
     marginTop: -10,
@@ -202,13 +454,12 @@ prompt: {
     flexDirection: "column",
     gap: 30,
     paddingHorizontal: 10,
-
     shadowColor: "#000000",
     shadowOffset: {
       width: 8,
       height: 7,
     },
-    shadowOpacity:  0.22,
+    shadowOpacity: 0.22,
     shadowRadius: 9.22,
     elevation: 12,
   },
@@ -238,5 +489,3 @@ prompt: {
 });
 
 export default Home;
-
-
